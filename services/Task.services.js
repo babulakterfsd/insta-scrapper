4f9d95374f9ed5369055ed8d0c0c849578db1796
Babul Akter <babulakterfsd@gmail.com>
2023-05-22 08:45:01 +0600

    get all username api added.

--- services/Task.services.js
+++ services/Task.services.js
@@ -1,16 +1,148 @@
+const puppeteer = require('puppeteer');
 const Task = require('../models/Task.model');
 
 module.exports.getAllTheUserNameService = async () => {
 
     let userNames = []
 
-   const users = await Task.find({}).limit(15)
+   const users = await Task.find({is_validated: false}).limit(5)
 
    users.forEach(user => {
-         userNames.push(user?.of_username.slice(1, 50).trim())
+         userNames.push(user?.ig_url.split('/')[3])
    })
 
    console.log(userNames);
+
+   // insta kaj karbar starts here
+
+   const emailRegex = /[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/;
+   const linkRegex = /"sameAs":"(.*?)"/;
+
+   function findEmail(obj) {
+      for (const key in obj) {
+          if (typeof obj[key] === 'object') {
+              const result = findEmail(obj[key]);
+              if (result) {
+                  return result;
+              }
+          } else if (typeof obj[key] === 'string') {
+              const emailMatch = emailRegex.exec(obj[key]);
+              if (emailMatch) {
+                  return emailMatch[0];
+              }
+          }
+      }
+      return null;
+  }
+  
+  function findLink(bioContent) {
+      const match = linkRegex.exec(bioContent);
+      const link = match ? match[1] : '';
+      if (link) {
+          return link.replace(/\\\//g, '/');
+      }
+      return null;
+  }
+
+  (async () => {
+      const browser = await puppeteer.launch({
+          headless: 'new',
+      });
+  
+      try {
+          for await (const username of userNames) {
+              try {
+                  const insta = await browser.newPage();
+                  await insta.goto(`https://instagram.com/${username}`, { waitUntil: 'networkidle0' });
+  
+                  const bioContent = await insta.evaluate(() => {
+                      const scriptTag = document.querySelector('script[type="application/ld+json"]');
+                      const jsonContent = scriptTag.textContent;
+                      return jsonContent;
+                  });
+  
+                  const data = JSON.parse(bioContent);
+  
+                  await insta.close();
+  
+                  const context = data['@context'];
+                  const type = data['@type'];
+                  const { description } = data;
+                  const authorType = data.author['@type'];
+                  const authorIdentifierType = data.author.identifier['@type'];
+                  const authorIdentifierPropertyID = data.author.identifier.propertyID;
+                  const authorIdentifierValue = data.author.identifier.value;
+                  const authorImage = data.author.image;
+                  const authorName = data.author.name;
+                  const authorAlternateName = data.author.alternateName;
+                  const authorSameAs = data.author.sameAs;
+                  const authorUrl = data.author.url;
+                  const mainEntityType = data.mainEntityOfPage['@type'];
+                  const mainEntityId = data.mainEntityOfPage['@id'];
+                  const identifierType = data.identifier['@type'];
+                  const identifierPropertyID = data.identifier.propertyID;
+                  const identifierValue = data.identifier.value;
+                  const interactionStatisticType1 = data.interactionStatistic[0]['@type'];
+                  const interactionStatisticInteractionType1 =
+                      data.interactionStatistic[0].interactionType;
+                  const interactionStatisticUserInteractionCount1 =
+                      data.interactionStatistic[0].userInteractionCount;
+                  const interactionStatisticType2 = data.interactionStatistic[1]['@type'];
+                  const interactionStatisticInteractionType2 =
+                      data.interactionStatistic[1].interactionType;
+                  const interactionStatisticUserInteractionCount2 =
+                      data.interactionStatistic[1].userInteractionCount;
+  
+                  const link = findLink(bioContent);
+                  const email = findEmail(data);
+  
+                  const result = {
+                      context,
+                      type,
+                      description,
+                      authorType,
+                      authorIdentifierType,
+                      authorIdentifierPropertyID,
+                      authorIdentifierValue,
+                      authorImage,
+                      authorName,
+                      authorAlternateName,
+                      authorSameAs,
+                      authorUrl,
+                      mainEntityType,
+                      mainEntityId,
+                      identifierType,
+                      identifierPropertyID,
+                      identifierValue,
+                      interactionStatisticType1,
+                      interactionStatisticInteractionType1,
+                      interactionStatisticUserInteractionCount1,
+                      interactionStatisticType2,
+                      interactionStatisticInteractionType2,
+                      interactionStatisticUserInteractionCount2,
+                      link,
+                      email,
+                  };
+  
+                  if (result?.email || result?.authorUrl) {
+                      console.log(result);
+                  } else {
+                      console.log(
+                          `The URL https://instagram.com/${result.authorAlternateName} is not valid`.red.bold
+                      );
+                  }
+              } catch (error) {
+                  console.error(`Error fetching Instagram data for username: ${username}`.red.bold);
+              }
+          }
+      } catch (error) {
+          console.error('Error:', error);
+      } finally {
+          await browser.close();
+      }
+  })();
+  
+
 
    return users
 }
