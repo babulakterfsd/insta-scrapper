4c7391720cd8e5e590ef896dd997172a1600ac7a
Babul Akter <babulakterfsd@gmail.com>
2024-07-14 13:16:59 +0600

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# instagram scarpper : get user's info from instagram withoout authentic api.
+# Insta - Web automation with puppeteer to check and validate users.
+
+## Features
+
+- Browser automation implemented with puppeteer
+- Advance filtering with mongoDB aggregation
+- Able to deal with 100k+ data without lagging
+
+## technologies:
+
+    - Node.js
+    - Express.js
+    - MongoDB
+    - Puppeteer
+    - Mongoose
+
+## How to run
+
+- First, clone the repo and install the dependencies using `yarn install` command.
+- then, build the project using `yarn run build` command.
+- at last, run the project using `yarn run dev` command.
+
+
--- services/Bioemail.services.js
+++ services/Bioemail.services.js
@@ -1,33 +1,41 @@
-const BioEmail = require('../models/Bioemail.model');
+const fs = require('fs');
+const path = require('path');
 const OfDataRawEmails = require('../models/Ofdatarawemails.model');
+const BioEmail = require('../models/Bioemail.model');
 
 
 module.exports.addNewBioEmail = async (req, res, next) => {
-     const AllUsersFromOfDataRawEmails = await OfDataRawEmails.find({}).select('of_url bio subscribers -_id')
-     let count = 0;
-     
-     AllUsersFromOfDataRawEmails.forEach(async (user) => {
+  const AllUsersFromOfDataRawEmails = await OfDataRawEmails.find({}).select('of_url bio subscribers -_id');
+  let count = 0;
+  const jsonData = []; 
+
+  AllUsersFromOfDataRawEmails.forEach(async (user) => {
+    if (user?.bio && user?.bio?.length > 0) {
+      const bioToBeTested = user.bio;
+      const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+      const emailsFromTheBio = bioToBeTested.match(emailRegex) || [];
+      
+      if (emailsFromTheBio?.length > 0) {
+        const userToBeCreatedInBioEmailsCollection = {
+          bio_email: emailsFromTheBio,
+          of_url: user?.of_url,
+          subscribers: user?.subscribers,
+        };
         
-         if(user?.bio && user?.bio?.length > 0) {
-              const bioToBeTested = user.bio;
-              const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
-              const emailsFromTheBio = bioToBeTested.match(emailRegex) || [];
-              if(emailsFromTheBio?.length > 0) {
-                const userToBeCreatedInBioEmailsCollection = {
-                    bio_email: emailsFromTheBio,
-                    of_url: user?.of_url,
-                    subscribers: user?.subscribers,
-                }
-                const userToBeSaved = new BioEmail(userToBeCreatedInBioEmailsCollection)
-                const result = await userToBeSaved.save()
-                console.log('data saved', count++);
-              }
-         } 
-     })
-}
+        jsonData.push(userToBeCreatedInBioEmailsCollection);
+      }
+    }
+  });
 
-module.exports.getAllBioEmails = async () => {
-    const result = await OfDataRawEmails.find({}).count()
-    return result
-}
-
+  const fileName = 'bioEmails.json';
+  const filePath = path.join(__dirname, '..', 'data', fileName);
+  const fileContent = JSON.stringify(jsonData, null, 2);
+  
+  fs.writeFile(filePath, fileContent, (err) => {
+    if (err) {
+      console.error('Error writing JSON file:', err);
+    } else {
+      console.log('JSON file created:', fileName);
+    }
+  });
+};
